f3658c65338a2fa67c9e72e62225534f413274d8
Boris Shevchenko <bshevchenkoweb@gmail.com>
2018-03-29 20:22:40 +0700

    SecurityToken, STO, TransferManager, PermissionManager classes exported for Flow typification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 export { default as PolyToken } from './contracts/PolyToken'
 export { default as SecurityTokenRegistry } from './contracts/SecurityTokenRegistry'
 export { default as TickerRegistry } from './contracts/TickerRegistry'
+export { default as SecurityToken } from './contracts/SecurityToken'
+export { default as STO } from './contracts/STO'
+export { default as TransferManager } from './contracts/TransferManager'
+export { default as PermissionManager } from './contracts/PermissionManager'
 export { default as CappedSTOFactory } from './contracts/CappedSTOFactory'
 
 export default from './contracts/Contract'
